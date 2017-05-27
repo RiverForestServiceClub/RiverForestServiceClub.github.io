f4c27d3292b4c9ddd9638dce2ce1a3b6b4ac1a93
Robert A. Zeh <robert.a.zeh@gmail.com>
2017-05-27 00:42:48 -0500

    Date update, and dash removal for John

--- bin/rfsc-sync.sh
+++ bin/rfsc-sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~/Documents/River-Forest-Service-Club/
+cd ~/src/RiverForestServiceClub.github.io/
 #lftp www.riverforestserviceclub.org -e "mirror --script=/home/razeh/foo -R --no-perms --exclude=".git" --exclude="stats""
 lftp -e "set ftp:ssl-allow off;" www.riverforestserviceclub.org -e "mirror -R --no-perms --exclude=".git" --exclude="stats""
 

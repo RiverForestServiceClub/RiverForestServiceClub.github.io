d07a35e659c9586b12045ba3d74a690081d8b749
Robert Allan Zeh <robert.a.zeh@gmail.com>
2019-01-26 16:28:32 -0600

    More renaming

--- bin/rfsc-sync.sh
+++ bin/rfsc-sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~/src/RiverForestServiceClub.github.io/
+cd ~/Documents/River-Forest-Service-Club/
 #lftp www.riverforestserviceclub.org -e "mirror --script=/home/razeh/foo -R --no-perms --exclude=".git" --exclude="stats""
 lftp -e "set ftp:ssl-allow off;" www.riverforestserviceclub.org -e "mirror -R --no-perms --exclude=".git" --exclude="stats""
 
